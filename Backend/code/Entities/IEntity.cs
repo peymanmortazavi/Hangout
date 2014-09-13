859b0f90d91d13e4723183ea1793e23b908fb7fd
mazdeh <vahid.mazdeh@gmail.com>
2014-09-12 18:56:34 -0600

    Added User Entitiy class, and fixed a bug in IDataAccess.cs

--- Backend/code/Entities/IEntity.cs
+++ Backend/code/Entities/IEntity.cs
@@ -7,6 +7,7 @@ namespace Hangout.Entities
 
 		string Id { get; set; }
 
+
 	}
 
 }
---
+++ Backend/code/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hangout.Entities
+{
+	public class User :IEntity
+	{
+		public string Id { get; set; }
+
+		public User ()
+		{
+
+		}
+
+	}
+}
