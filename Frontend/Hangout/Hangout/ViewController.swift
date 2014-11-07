2ce1160f457efde50f2c04c0795736259e7b3471
Peyman Mortazavi <pmortazavi@cailmobility.com>
2014-11-06 19:49:51 -0700

    Initial Commit

---
+++ Frontend/Hangout/Hangout/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Hangout
+//
+//  Created by Peyman Mortazavi on 11/6/14.
+//  Copyright (c) 2014 Peyman Mortazavi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var connection: SRConnection = SRConnection(URL: "http://localhost:8080/test");
+        connection.start()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
