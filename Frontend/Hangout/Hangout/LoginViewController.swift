3c5fd45820752bc0246fffdc1bf6cb503a177ad6
Becquerine <ryan.tabler@colorado.edu>
2014-12-02 00:51:07 -0700

    Downgraded AFNetworking version in podfile to 1.2.0 to get it to build. Added connection checker so app doesn't crash when it can't find the server.

--- Frontend/Hangout/Hangout/LoginViewController.swift
+++ Frontend/Hangout/Hangout/LoginViewController.swift
@@ -13,6 +13,10 @@ import UIKit
 class LoginViewController: UIViewController {
     @IBOutlet weak var firstNameLabel: UILabel!
     
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    // Currently trying to get dismiss the keyboard when user taps out. I know I need to call emailTextField.resignFirstResponder at the right time
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
