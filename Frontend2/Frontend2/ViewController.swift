090769a8d2adbe3573f43a2d87c75883637b0530
Becquerine <ryan.tabler@colorado.edu>
2014-12-08 16:04:48 -0700

    Added Frontend2 directory with blank Xcode project

---
+++ Frontend2/Frontend2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Frontend2
+//
+//  Created by Ryan Tabler on 12/8/14.
+//  Copyright (c) 2014 Ryan Tabler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
